691db4a665e54b8f23a6c7f39d1ce9713e73bb7f
Magnus Mickelsson <magnus.mickelsson@gmail.com>
2015-03-29 22:21:23 +0200

    Spored: Added "Skyterror" Personal Delivery System detection

--- Spored/Spored.lua
+++ Spored/Spored.lua
@@ -18,6 +18,8 @@ EventFrame:RegisterEvent("UNIT_AURA")
 
 -- Data structure for storing detected feather usage. Since it's a refreshable unit buff it spams the chat otherwise.
 local recentFeathers = {}
+local terrorBuffText = "\"Skyterror\" Personal Delivery System";
+local featherBuffText = "Aviana's Feather";
 
 -- Event Handler
 EventFrame:SetScript("OnEvent", function(self,event,...)
@@ -50,29 +52,44 @@ EventFrame:SetScript("OnEvent", function(self,event,...)
 			c_debug("Interrupt cast " .. tostring(spellId) .. " " .. tostring(spellName) .. " by " .. tostring(sourceName) .. " on " .. tostring(destName));
 		end
 
+--		c_debug("Spell ID: " .. tostring(spellId));
+--		c_debug("Source: " .. tostring(sourceName));
+--		c_debug("Target: " .. tostring(destName));
+		
 	elseif (event == "UNIT_AURA") then
 		local unit=...
 		
-        if UnitBuff(unit,"Aviana's Feather") then
+		local featherBuff = UnitBuff(unit, featherBuffText);
+		local terrorBuff = UnitBuff(unit, terrorBuffText);
+		local buff = nil;
+		
+		if (featherBuff) then
+			buff = featherBuffText;
+		elseif (terrorBuff) then
+			buff = terrorBuffText;
+		end
+		
+        if (featherBuff or terrorBuff) then
 			local now = GetTime();
 			local unitName = GetUnitName(unit, true)
 			local unitNameWithRealm = unitName
 			
-			c_debug("Aviana's feather buff detected on " .. unitNameWithRealm .. " at time: " .. now);
+			c_debug(buff .. " detected on " .. unitNameWithRealm .. " at time: " .. now);
 			
 			local unitRecentlyGotFeatherBuff = false
 			local numberOfRecentFeathers = 0
 			local unitFoundAmongRecent = false
 			
+			-- todo: Use two separate storages for buffs? Seems inefficient. Maybe we can assume within 60 sec only one of them is used.
 			for k,v in pairs(recentFeathers) do
 				numberOfRecentFeathers = numberOfRecentFeathers + 1;
 				if k == unitNameWithRealm then
 					unitFoundAmongRecent = true;
 					local timeSinceUsed = now - v;
-					c_debug("Unit " .. unitNameWithRealm .. " recently got feathers buff: " .. timeSinceUsed);
+					c_debug("Unit " .. unitNameWithRealm .. " recently got " .. buff .. ": " .. timeSinceUsed);
 
 					if timeSinceUsed > 60 then
-						c_debug("Unit " .. unitNameWithRealm .. " had more than 60s since last feathers buff, resetting start.");
+						c_debug("Unit " .. unitNameWithRealm .. " had more than 60 since last buff, resetting start.");
 						recentFeathers[unitNameWithRealm] = now
 					else
 						unitRecentlyGotFeatherBuff = true
@@ -85,13 +102,13 @@ EventFrame:SetScript("OnEvent", function(self,event,...)
 			end
 			
 			if unitRecentlyGotFeatherBuff == false then
-				c_print("SPORED: Aviana's Feather used by " .. tostring(unitNameWithRealm));
+				c_print("SPORED: " .. buff .. " used by " .. tostring(unitNameWithRealm));
 			end
         end
 	end
 end)
 
 -- Add slash command /spored
-SlashCmdList["SPORED"] = function() c_print("SPORED active. Written by Sorny of Defias Brotherhood. Currently detects Sinister Spores, Super Sticky Glitter Bomb and Aviana's Feather usage."); end
+SlashCmdList["SPORED"] = function() c_print("SPORED active. Written by Sorny of Defias Brotherhood. Currently detects Sinister Spores, Glitter bomb, Skyterror and Aviana's Feather usage. How to use? As-is. No config atm, just LUA."); end
 SLASH_SPORED1 = '/spored'
 
